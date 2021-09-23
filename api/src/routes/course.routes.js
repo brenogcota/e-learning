58c7da802246009d0d558758adae0a5762f5339e
brenogcota <brenocota4@gmail.com>
2021-09-23 17:03:12 -0300

    feat: create course

--- api/src/routes/course.routes.js
+++ api/src/routes/course.routes.js
@@ -3,6 +3,7 @@ const CourseService = require('../services/Course');
 
 const route = express.Router();
 
-route.get('/:course', CourseService.index);
+route.post('/', CourseService.create);
+route.get('/:course', CourseService.show);
 
 module.exports = route;
--- api/src/services/Course/index.js
+++ api/src/services/Course/index.js
@@ -1,8 +1,12 @@
 
-const { getRepository, Like } = require('typeorm');
+const { getRepository } = require('typeorm');
+const { v4 } = require('uuid');
 const Course = require('../../entity/Course');
 
-const index = async (req, res, next) => {
+const slugfy = require('../../utils/slugfy');
+const logger = require('../../infra/logger');
+
+const show = async (req, res, next) => {
     try {
         const courseRepository = getRepository(Course);
         const name = req.params.course;
@@ -23,6 +27,33 @@ const index = async (req, res, next) => {
     }
 }
 
+const create = async (req, res, next) => {
+    try {
+        const { name, details, lessons, userId } = req.body;
+
+        const courseRepository = getRepository(Course);
+        let course = await courseRepository.findOne({ where: { name } });
+
+        if(course) return res.status(403).json({ message: 'Course already exists.' });
+
+        course = {
+            id: v4(),
+            name: slugfy(name),
+            details,
+            lessons,
+            userId
+        }
+
+        course = await courseRepository.save(course);
+
+        res.status(200).json(course);
+    } catch(err) {
+        logger.error(err);
+        next(err);
+    }
+}
+
 module.exports = {
-    index
+    show,
+    create
 }
